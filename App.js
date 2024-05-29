6de883ab545d6e349c02c087547ec21985e91c0b
logancol <lcolson1@ufl.edu>
2024-05-29 16:52:19 -0400

    Skeleton

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello react native</Text>
       <StatusBar style="auto" />
     </View>
   );
