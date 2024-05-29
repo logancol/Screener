5d798ab85af456a35c6f67523332447c8f1fa7f2
logancol <lcolson1@ufl.edu>
2024-05-29 17:50:06 -0400

    basic opening dialogue screen

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Animated, Text, View, ImageBackground } from 'react-native';
+
+const FadeInOutText = ({ text, duration, delay }) => {
+  const [fadeAnim] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: duration,
+        useNativeDriver: true,
+        delay: delay // Add delay for the fade-in effect
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: duration,
+        useNativeDriver: true,
+        delay: duration // Add delay for the fade-out effect
+      })
+    ]).start();
+  }, []);
+
+  return (
+    <Animated.View style={{ opacity: fadeAnim }}>
+      <Text style={styles.text}>{text}</Text>
+    </Animated.View>
+  );
+};
 
 export default function App() {
-  
   return (
-    <View style={styles.container}>
-      <Text>Hello react native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/gradient wpp.png')}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <FadeInOutText text="This is your 19th time attempting to open Instagram today" duration={2000} delay={0} />
+        <FadeInOutText text="That's pathetic" duration={2000} delay={4000} />
+        <FadeInOutText text="You need to do better" duration={2000} delay={8000} />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
